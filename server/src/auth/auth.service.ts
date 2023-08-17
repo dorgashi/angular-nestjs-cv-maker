c4d554f5c4096293f376d8fbc22ba37b65f700e3
Dorant Gashi <gashidorant@gmail.com>
2023-08-17 13:15:41 +0200

    Added AuthGuard and currentUser decorator

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -3,6 +3,12 @@ import { JwtService } from '@nestjs/jwt';
 import * as argon2 from 'argon2';
 import { UserService } from '../user/user.service';
 import { LoginUserDto } from './dto/login-user.dto';
+import { User } from '../user/user.entity';
+
+type JwtPayload = {
+    sub: number;
+    [key: string]: any;
+} | null;
 
 @Injectable()
 export class AuthService {
@@ -30,7 +36,25 @@ export class AuthService {
         };
     }
 
-    decodeToken(token: string): string | { [key: string]: any } | null {
-        return this.jwtService.decode(token, { json: true });
+    decodeToken(token: string): JwtPayload | null {
+        const decoded = this.jwtService.decode(token, {
+            json: true,
+        }) as JwtPayload;
+
+        if (!decoded || !decoded.sub) return null;
+
+        return decoded;
+    }
+
+    async verifyTokenAndRetrieveUser(token: string): Promise<User | boolean> {
+        const valid = await this.jwtService.verify(token);
+
+        if (!valid) return false;
+
+        const decoded = this.decodeToken(token);
+
+        if (!decoded) return false;
+
+        return this.userService.findById(decoded.sub);
     }
 }
